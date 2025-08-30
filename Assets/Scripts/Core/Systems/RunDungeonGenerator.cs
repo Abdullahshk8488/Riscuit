cd559310f9b22b8e9d62ef857a2e0556ab850611
Abdullah Sheikh <abdullahsheikh1998@gmail.com>
2025-08-30 14:50:39 -0700

    Added player to the level

--- Assets/Scripts/Core/Systems/RunDungeonGenerator.cs
+++ Assets/Scripts/Core/Systems/RunDungeonGenerator.cs
@@ -7,5 +7,9 @@ public class RunDungeonGenerator : MonoBehaviour
     private void Start()
     {
         abstractDungeonGenerator.GenerateDungeon();
+        Player player = PlayerManager.Instance.MainPlayer;
+        RoomManager roomManager = RoomManager.Instance;
+        Transform startPoint = roomManager.GetPlayerSpawnPoint();
+        player.transform.position = startPoint.position;
     }
 }
