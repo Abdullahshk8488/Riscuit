edb51e3e756d9148d4b7fee3e999cb8c9a441282
Emilian02 <emijurflo@gmail.com>
2025-08-30 02:43:59 -0700

    Long range enemy working

--- Assets/Scripts/Core/Enemies/LongEnemyBullet.cs
+++ Assets/Scripts/Core/Enemies/LongEnemyBullet.cs
@@ -4,6 +4,18 @@ public class LongEnemyBullet : BaseIcing
 {
     [SerializeField] private Collider2D bulletCollider;
 
+    private GameObject _player;
+
+    public override void Shoot(Vector2 direction)
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        Vector2 toPlayer = ((Vector2)_player.transform.position - (Vector2)transform.position).normalized;
+
+        float angle = Mathf.Atan2(toPlayer.y, toPlayer.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        base.Shoot(direction);
+    }
+
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
